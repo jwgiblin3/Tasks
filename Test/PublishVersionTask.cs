d9fc12d1d54e85ad008791ad0b4be32a477def9a
jwgiblin3 <jwgiblin3@hotmail.com>
2015-10-20 22:31:35 -0400

    aa

---
+++ Test/PublishVersionTask.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using Microsoft.Build.Utilities;
+using Microsoft.Build.Framework;
+using System.Diagnostics;
+
+
+namespace Test
+{
+    class PublishVersionTask : Task
+    {
+        //static int rev = 1;
+        //static DateTime currentdate = DateTime.Now;
+
+        public int Revision
+        {
+            get
+            {
+                //if (DateTime.Now.Date == currentdate.Date)
+                //    rev++;
+                //else
+                //    rev = 1;
+                //return rev;
+                return 1;
+            }
+        }
+
+        [Output]
+        public string PublishVersion
+        {
+            get
+            {
+                return DateTime.Now.ToString("yyyy.MM.dd.") + Revision.ToString();
+
+            }
+        }
+
+        public override bool Execute()
+        {
+            Debug.WriteLine("PublishVersion = " + PublishVersion);
+            return true;
+        }
+
+
+
+    }
+}
